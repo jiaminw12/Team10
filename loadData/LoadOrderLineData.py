04194e58c4446d2ea48137f5fff26962a2704ec5
Jia Min <jiaminw@ymail.com>
2017-09-24 23:00:02 +0800

    Updated LoadOrderLineData

--- loadData/LoadOrderLineData.py
+++ loadData/LoadOrderLineData.py
@@ -2,7 +2,10 @@
 import csv
 from decimal import *
 from datetime import datetime, date, time
+import time
 from cassandra.cluster import Cluster
+import sys
+
 
 def getdata(filename):
     with open(filename, "rb") as csvfile:
@@ -14,12 +17,13 @@ def getdata(filename):
 cluster = Cluster();
 session = cluster.connect('team10')
 
-update_statement = session.prepare("UPDATE orderline SET o_c_id = ?, o_all_local = ?, o_carrier_id = ?, o_entry_d = ?, o_ol_cnt = ? WHERE o_w_id = ? AND o_d_id = ? AND o_id = ?");
 
-print(datetime.now())
+select_statement = session.prepare("SELECT count(ol_number) AS count FROM orderline WHERE o_w_id = ? AND o_d_id = ? AND o_id = ?");
 
 print("Updating OderLine data ... ")
+#start_time = time.time()
 for row in getdata('../data-files/order.csv'):
+
     o_c_id = int(row[3])
     o_all_local = Decimal(row[6])
     if row[4] != 'null':
@@ -31,8 +35,21 @@ for row in getdata('../data-files/order.csv'):
     o_w_id = int(row[0])
     o_d_id = int(row[1])
     o_id = int(row[2])
-    
+
+    result_count = session.execute(select_statement, [o_w_id, o_d_id, o_id])
+    for row in result_count:
+        ol_number = int(row[0])
+
+    str_list = []
+    for i in range (1, ol_number+1):
+        str_list.append(`i`)
+        str = ','.join(str_list)
+
+    update_statement = session.prepare("UPDATE orderline SET o_c_id = ?, o_all_local = ?, o_carrier_id = ?, o_entry_d = ?, o_ol_cnt = ? WHERE o_w_id = ? AND o_d_id = ? AND o_id = ? AND ol_number IN (" +  str + ")");
+
     session.execute(update_statement, [o_c_id, o_all_local, o_carrier_id, o_entry_d, o_ol_cnt, o_w_id, o_d_id, o_id])
+
+#print("--- %s seconds ---" % round(time.time() - start_time, 10))
 print("Updating Done ... ")
 
 cluster.shutdown();
