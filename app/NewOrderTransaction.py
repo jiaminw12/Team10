0d3f2c4087177513959c22a0a6a25c17a409f712
wireless90 <wireless-@hotmail.com>
2017-09-27 15:49:35 +0800

    Done Constructor of New Order Transaction

--- app/NewOrderTransaction.py
+++ app/NewOrderTransaction.py
@@ -4,14 +4,15 @@ from cassandra.cluster import Cluster
 
 class NewOrderTransaction():
 
-    def __init__(self, w_id, d_id, c_id, num_items, item_num, supplier_warehouse, quantity):
+    def __init__(self, session, w_id, d_id, c_id, num_items, i_id_list, supplier_w_id_list, quantity_list):
+        self.session = session
         self.w_id = w_id
         self.d_id = d_id
         self.c_id = c_id
         self.num_items = num_items
-        self.item_num = item_num
-        self.supplier_warehouse = supplier_warehouse
-        self.quantity = quantity
+        self.i_id_list = i_id_list
+        self.supplier_w_id_list = supplier_w_id_list
+        self.quantity_list = quantity_list
 
     def process(self):
     # Need to update/insert record orderline & delivery_by_customer when is related to order or orderline
