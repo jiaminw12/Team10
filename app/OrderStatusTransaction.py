23040c7bec9aee499028cc57590626af138592f5
Jia Min <jiaminw@ymail.com>
2017-10-04 15:43:24 +0800

    Fixed bug in Order-Status & Stock-Level

--- app/OrderStatusTransaction.py
+++ app/OrderStatusTransaction.py
@@ -2,38 +2,39 @@
 
 from cassandra.cluster import Cluster
 from time import gmtime, strftime
-class OrderStatusTransaction(object):
-    
-    def __init__(self, c_w_id, c_d_id, c_id):
-        self.c_w_id = c_w_id
-        self.c_d_id = c_d_id
-        self.c_id = c_id
-    self.process()
-    
-    def process(self):
-        # get the customer's last order
-        select_customer_las_order = session.prepare("SELECT c_first, c_middle, c_last, c_balance from PSYMENT_BY_CUSTOMER WHERE w_id = ? and d_id = ? and c_id = ?")
-        
-        # get the last order id
-        select_last_order = session.prepare("select o_id from delivery_by_customer where w_id = ? AND d_id = ? AND c_id = ? Limit 1")
-        orderid = session.execute(select_last_order,(self.c_w_id, self.c_d_id, self_c_id))
-        lastorder = orderid[0]
-        
-        select_order = session.prepare("SELECT o_id,o_entry_d, o_carrier_id, ol_i_id, ol_supply_w_id, ol_quantity,ol_amount,ol_delivery_d from delivery_by_customer where w_id = ? AND d_id = ? AND c_id = ? AND o_id = ?")
-        # execute
-        customerdetails = session.execute(self.select_customer_las_order,(self.w_id,self.c_d_id,self.c_id));
-        # print output 1. customer name and balance 2. its order
-        customer = customerdetails[0]
-        print "Customer name: %s %s %s, Balance amount: %f\n"(customer.c_first, customer.c_middle, customer.c_last, customer.c_balance)
 
-        # print order with items
-        otderdetails = session.execute(self.select_order,(self.c_w_id,self.c_d_id,self.c_id, lastorder.o_id))
-        length = len(otderdetails)
-        temp = otderdetails[0];
-        print "order ID: %d\t Entry Date: %s\t Carrier ID: %d\t\n"(temp.o_id, temp.o_entry_d,temp.o_carrier_id)
-            for x in range(0,length):
-                row = otderdetails[x]
-                print "Item ID: %d\t Supply Warehouse ID: %d\t Quantity: %d\t Amount: %f\t Delivery Date: %s\n"(row.ol_i_id, row.ol_supply_w_id, row.ol_quantity, row.ol_amount,row.ol_delivery_d)
+class OrderStatusTransaction(object):
+
+	def __init__(self, session, c_w_id, c_d_id, c_id):
+		self.session = session
+		self.c_w_id = c_w_id
+		self.c_d_id = c_d_id
+		self.c_id = c_id
+
+	def process(self):
+		# get the customer's last order
+		select_customer_las_order = self.session.prepare("SELECT c_first, c_middle, c_last, c_balance from payment_by_customer WHERE c_w_id = ? and c_d_id = ? and c_id = ?")
+
+		# get the last order id
+		select_last_order = self.session.prepare("select o_id from delivery_by_customer where o_w_id = ? AND o_d_id = ? AND o_c_id = ? LIMIT 1 ALLOW FILTERING")
+		orderid = self.session.execute(select_last_order, (int(self.c_w_id), int(self.c_d_id), int(self.c_id)))
+		lastorder = orderid[0]
+
+		select_order = self.session.prepare("SELECT o_id, o_entry_d, o_carrier_id, ol_i_id, ol_supply_w_id, ol_quantity, ol_amount, ol_delivery_d from delivery_by_customer where o_w_id = ? AND o_d_id = ? AND o_c_id = ? AND o_id = ? ALLOW FILTERING")
+		
+		# execute
+		customerdetails = self.session.execute(select_customer_las_order, (int(self.c_w_id), int(self.c_d_id), int(self.c_id)));
+		# print output 1. customer name and balance 2. its order
+		customer = customerdetails[0]
+		print "Customer name: %s %s %s, Balance amount: %2f\n"%(customer.c_first, customer.c_middle, customer.c_last, customer.c_balance)
+
+		# print order with items
+		orderdetails = self.session.execute(select_order,(int(self.c_w_id), int(self.c_d_id), int(self.c_id), int(lastorder.o_id)))
+		temp = orderdetails[0];
+		print "order ID: %d\t Entry Date: %s\t Carrier ID: %d\t\n" % (temp.o_id, temp.o_entry_d,temp.o_carrier_id)
+		
+		for row in orderdetails:
+			print "Item ID: %d\t Supply Warehouse ID: %d\t Quantity: %d\t Amount: %f\t Delivery Date: %s\n" % (row.ol_i_id, row.ol_supply_w_id, row.ol_quantity, row.ol_amount,row.ol_delivery_d)
 
 
 
--- app/StockLevelTransaction.py
+++ app/StockLevelTransaction.py
@@ -4,33 +4,33 @@ from cassandra.cluster import Cluster
 from time import gmtime, strftime
 
 class StockLevelTransaction(object):
-    
-    def __init__(self, w_id, d_id, stockThreshold, numOfLastOrder):
-        self.w_id = w_id
-        self.d_id = d_id
-        self.stockThreshold = stockThreshold
-        self.numOfLastOrder = numOfLastOrder
-    
-    def process(self):
-        # prepare statement
-        # get the next_o_id N
-        select_d_next_o_id = session.prepare("SELECT D_NEXT_O_ID FROM ITEM_BY_WAREHOUSE_DISTRICT WHERE W_ID = ? AND D_ID = ?")
-        # get set of items from the last L orders
-        select_last_l_order = session.prepare("SELECT OL_I_ID FROM ORDER_LINE WHERE O_W_ID = ? AND O_D_ID = ? AND O_O_ID >= (? - ?) AND O_ID < ?")
 
-        # get total number of items in S where its stock quantity < T
-        select_quantity = session.prepare("SELECT count(distinct I_id)as countno FROM ITEM_BY_WAREHOUSE_DISTRICT WHERE I_ID = ? AND S_QUANTITY < ?")
+	def __init__(self, session, w_id, d_id, stockThreshold, numOfLastOrder):
+		self.session = session
+		self.w_id = w_id
+		self.d_id = d_id
+		self.stockThreshold = stockThreshold
+		self.numOfLastOrder = numOfLastOrder
 
-        # execute
-        next_oid = session execute(select_d_next_o_id,(self.w_id, self.d_id))
-        next_o_id = next_oid[0]
-        last_l_orders = session.execute(select_last_l_order,(self.w_id,self.d_id,self.next_o_id,self.numOfLastOrder, self.next_o_id))
-        length = len(last_l_orders)
-            for x in range (0, length):
-                row = last_l_orders[x]
-                execute_count = session.execute(select_quantity,(row.OL_I_ID, self.stockThreshold))
-                    if execute_count>0:
-                        print "Item ID: %d \nNumber: %d\n"(row.OL_I_ID, execute_count.countno)
+	def process(self):
+		# prepare statement
+		# get the next_o_id N
+		select_d_next_o_id = self.session.prepare("SELECT D_NEXT_O_ID FROM district WHERE d_w_id = ? AND d_id = ?")
+		# get set of items from the last L orders
+		select_last_l_order = self.session.prepare("SELECT OL_I_ID FROM orderline WHERE O_W_ID = ? AND O_D_ID = ? AND O_ID >= ? AND O_ID < ?")
+
+		# get total number of items in S where its stock quantity < T
+		select_quantity = self.session.prepare("SELECT count(i_id)as countno FROM ITEM_BY_WAREHOUSE_DISTRICT WHERE w_id = ? AND i_id = ? AND s_quantity < ? ALLOW FILTERING")
+
+		# execute
+		next_oid = self.session.execute(select_d_next_o_id, (int(self.w_id), int(self.d_id)))
+		next_o_id = next_oid[0].d_next_o_id
+		
+		last_l_orders = self.session.execute(select_last_l_order,(int(self.w_id), int(self.d_id), (int(next_o_id) - int(self.numOfLastOrder)), int(next_o_id)))
+		for row in last_l_orders:
+			execute_count = self.session.execute(select_quantity, (int(self.w_id), int(row.ol_i_id), int(self.stockThreshold)))
+			if execute_count[0].countno > 0:
+				print "Item ID: %d \nNumber: %d\n"%(row.ol_i_id, execute_count[0].countno)
 
 
 
