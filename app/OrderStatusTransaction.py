1601ad56405ef1164e795d2a47fa037217b5a41f
feifeiliang <a0125994@u.nus.edu>
2017-10-13 01:38:10 +0800

    Update OrderStatusTransaction.py
    1. call customer table to get customer names and balance
    2. call OrderByDesc limit 1 to get customer's last order
    3. call Orderline table to output items details

--- app/OrderStatusTransaction.py
+++ app/OrderStatusTransaction.py
@@ -19,9 +19,9 @@ class OrderStatusTransaction(object):
 		self.select_customer_las_order = self.session.prepare("SELECT c_first, c_middle, c_last, c_balance from payment_by_customer WHERE c_w_id = ? and c_d_id = ? and c_id = ?")
 		
 		# get the last order id
-		self.select_last_order = self.session.prepare("select o_id from delivery_by_customer where o_w_id = ? AND o_d_id = ? AND o_c_id = ? LIMIT 1 ALLOW FILTERING")
+		self.select_last_order = self.session.prepare("select o_id, o_entry_d, o_carrier_id from OrderByDesc where o_w_id = ? AND o_d_id = ? AND o_c_id = ? LIMIT 1 ALLOW FILTERING")
 	
-		self.select_order = self.session.prepare("SELECT o_id, o_entry_d, o_carrier_id, ol_i_id, ol_supply_w_id, ol_quantity, ol_amount, ol_delivery_d from delivery_by_customer where o_w_id = ? AND o_d_id = ? AND o_c_id = ? AND o_id = ? ALLOW FILTERING")
+		self.select_order = self.session.prepare("SELECT ol_i_id, ol_supply_w_id, ol_quantity, ol_amount, ol_delivery_d from OrderLine where ol_w_id = ? AND ol_d_id = ? AND ol_c_id = ? AND ol_o_id = ? ALLOW FILTERING")
 	
 		if self.consistencyLevel == '1' :
 			self.select_customer_las_order.consistency_level = ConsistencyLevel.ONE
@@ -47,9 +47,9 @@ class OrderStatusTransaction(object):
 			print "Customer name: %s %s %s, Balance amount: %2f\n"%(customer.c_first, customer.c_middle, customer.c_last, customer.c_balance)
 
 			# print order with items
-			orderdetails = self.session.execute(self.select_order,(int(self.c_w_id), int(self.	c_d_id), int(self.c_id), int(lastorder.o_id)))
+			orderdetails = self.session.execute(self.select_order,(int(self.c_w_id), int(self.c_d_id), int(self.c_id), int(lastorder.o_id)))
 			temp = orderdetails[0];
-			print "order ID: %d\t Entry Date: %s\t Carrier ID: %s\t\n" % (temp.o_id, temp.o_entry_d, temp.o_carrier_id)
+			print "order ID: %d\t Entry Date: %s\t Carrier ID: %s\t\n" % (lastorder.o_id, lastorder.o_entry_d, lastorder.o_carrier_id)
 		
 			for row in orderdetails:
 				print "Item ID: %d\t Supply Warehouse ID: %d\t Quantity: %d\t Amount: %f\t Delivery Date: %s\n" % (row.ol_i_id, row.ol_supply_w_id, row.ol_quantity, row.ol_amount,row.ol_delivery_d)
